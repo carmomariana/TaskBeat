e33f489dde7185b9e911b6926623b2341533c41f
carmomariana <mariana18oc@gmail.com>
2024-05-20 19:10:08 -0300

    TaskEntity

--- app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
@@ -1,9 +1,12 @@
 package com.devspace.taskbeats
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     val category: String,
     val name: String,
 )
